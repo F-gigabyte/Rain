a02dbf71a2e8de71f1f2ca41f04ed0e1aa19106d
F-gigabyte <ftlg987@gmail.com>
2024-02-24 20:30:51 +0000

    Debugging bytecode

---
+++ src/debug.c
@@ -0,0 +1,35 @@
+#include <debug.h>
+#include <stdio.h>
+
+void disassemble_chunk(Chunk* chunk, const char* name)
+{
+    printf("== %s ==\n", name);
+    for(size_t off = 0; off < chunk->size;)
+    {
+        off = disassemble_inst(chunk, off);
+    }
+}
+
+static size_t simple_inst(const char* name, size_t offset)
+{
+    printf("%s\n", name);
+    return offset + 1;
+}
+
+size_t disassemble_inst(Chunk* chunk, size_t offset)
+{
+    printf("%04zu ", offset);
+    inst_type inst = chunk->code[offset];
+    switch(inst)
+    {
+        case OP_RETURN:
+        {
+            return simple_inst("OP_RETURN", offset);
+        }
+        default:
+        {
+            printf("Unknown opcode %d\n", inst);
+            return offset + 1;
+        }
+    }
+}
